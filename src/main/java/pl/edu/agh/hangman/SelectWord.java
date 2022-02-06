18eae08cc2e0bb8342139bad9f2ea1ed25261223
CodeWithN <ngmiterek@vp.pl>
2022-02-06 14:09:15 +0100

    Selection of word from new class

---
+++ src/main/java/pl/edu/agh/hangman/SelectWord.java
@@ -0,0 +1,36 @@
+package pl.edu.agh.hangman;
+
+
+import java.io.File;
+import java.util.Random;
+import java.util.Scanner;
+
+public class SelectWord {
+
+
+   // public static void main(String[] args) {
+     //   new SelectWord().selectWord();
+      //  System.out.println(new SelectWord().selectWord());
+  // }
+    public String selectWord() {
+
+        File f = new File("src/main/resources/slowa.txt");
+
+        try (Scanner scanner = new Scanner(f)) {
+            int index = new Random().nextInt(1600);
+
+            int i = 0;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                i++;
+                if (i == index) {
+                    return line;
+                }
+            }
+
+        } catch (Exception ex) {
+            System.out.println();
+
+        } return "";
+    }
+}
