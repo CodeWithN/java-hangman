e683c89dd950ef4fe08ea297b80b8a6582b0297f
BM <mbal@wp.pl>
2022-02-06 11:04:50 +0100

    SelectWord

--- src/test/java/pl/edu/agh/hangman/SelectWord.java
+++ src/test/java/pl/edu/agh/hangman/SelectWord.java
@@ -2,26 +2,20 @@ package pl.edu.agh.hangman;
 
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class SelectWord {
 
 
-    File file = new File("slowa.txt");
-    int index=0;
+    private void selectWord() {
 
-    try {
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-            String word = scanner.nextLine();
-                    index++;
-                    Random random = new Random();
-                    int w = random.nextInt(index);
-                    System.out.println(index);
+        File f = new File("resources/slowa.txt");
+        try (Scanner scanner = new Scanner(f)) {
+            String word = scanner.next();
+
+        } catch (Exception ex) {
+            System.out.println("INFO No valid conf data");
         }
-
-    } catch
+    }
 }
-
